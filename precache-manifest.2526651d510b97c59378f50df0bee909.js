e5e31cde28c1eaf3934cdfccd03c00cece2abb13
haqisaurus <haqisaurus@yahoo.com>
2020-04-12 21:48:47 +0700

    Updates

---
+++ precache-manifest.2526651d510b97c59378f50df0bee909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31106fb4d74c685146f85c97cafc839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0788b3f17323ee9b9aa4",
+    "url": "/static/css/2.e86c4c73.chunk.css"
+  },
+  {
+    "revision": "2108d44a855149b763f1",
+    "url": "/static/css/main.ccf36666.chunk.css"
+  },
+  {
+    "revision": "0788b3f17323ee9b9aa4",
+    "url": "/static/js/2.e19228ba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e19228ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2108d44a855149b763f1",
+    "url": "/static/js/main.2e7ee15f.chunk.js"
+  },
+  {
+    "revision": "d644ff198816bb9f0669",
+    "url": "/static/js/runtime-main.c0627188.js"
+  }
+]);
